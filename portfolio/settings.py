550af4ff1d23667dcb1e76c96bbca14034bd7a76
Arib61 <aribaymane61@gmail.com>
2024-07-25 16:05:38 +0000

    Update requirements and settings for mysql-connector-python

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -2,6 +2,13 @@ import os
 from pathlib import Path
 import environ
 
+env = environ.Env(
+    # Définir des valeurs par défaut pour les variables d'environnement
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost'])
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
